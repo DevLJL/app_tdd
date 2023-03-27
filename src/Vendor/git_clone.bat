7ff2778519955462a12ebb2af494d60984449702
DevLJL <125483234+DevLJL@users.noreply.github.com>
2023-03-27 13:05:34 -0300

    .

--- src/Vendor/git_clone.bat
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/ezequieljuliano/DataSetConverter4Delphi.git
-git clone https://github.com/viniciussanchez/dataset-serialize.git
-git clone https://github.com/DevLJL/SmartPointer.git
-git clone https://github.com/DevLJL/ZLConnection.git
-pause
